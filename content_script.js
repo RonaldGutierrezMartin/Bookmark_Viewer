855456f95bdf30d4df781772e43338fed8fe0ee8
Ronald <ronaldgutierrezmartin02@gmail.com>
2023-04-22 23:54:12 +0100

    ya tengo el bucle que recorre todas la listas

--- content_script.js
+++ content_script.js
@@ -3,7 +3,7 @@ const botonTools = document.querySelector("#botonTools");
 const templatePlantilla = document.querySelector("#templatePlantilla");
 const fragmento = document.createDocumentFragment();
 const templatePlantillaClon = templatePlantilla.cloneNode(true);
-const mainContendor = document.querySelector("main")
+const mainContendor = document.querySelector("main");
 botonTools.addEventListener("click", () => {
   //   chrome.bookmarks.create({
   //     title: "G4",
@@ -16,21 +16,34 @@ botonTools.addEventListener("click", () => {
   miPromesa
     .then((resultado) => {
       const templatePlantilla = document.querySelector("#templatePlantilla");
-      const fragmento = document.createDocumentFragment()
-      const templatePlantillaClon = templatePlantilla.content.cloneNode(true);
+      const fragmento = document.createDocumentFragment();
+      
 
-      console.log(templatePlantillaClon)
+      // console.log(templatePlantillaClon);
 
-      const array = resultado[0]
-      console.log(array.children[0].children[20].title)
-      const objetoWeb = array.children[0].children[20]
-      console.log(objetoWeb)
-      templatePlantillaClon.querySelector("#tituloTarjeta").textContent = objetoWeb.title
-      fragmento.appendChild(templatePlantillaClon)
-      mainContendor.appendChild(fragmento)
+      // const array = resultado[0];
+      // console.log(resultado);
+      // console.log(array.children[0].children[20].title);
+      // const objetoWeb = array.children[0].children[20];
+      // console.log(objetoWeb);
+
+      resultado.forEach((arrayAbuelo) => {
+        arrayAbuelo.children.forEach((arrayHijo) => {
+          arrayHijo.children.forEach((i) => {
+            console.log(i);
+            const templatePlantillaClon = templatePlantilla.content.cloneNode(true);
+            templatePlantillaClon.querySelector(".tituloTarjeta").textContent =
+              i.title;
+            fragmento.appendChild(templatePlantillaClon);
+            
+          });
+        });
+      });
+
+      mainContendor.appendChild(fragmento);
     })
     .catch((error) => {
       // Manejo de errores si la promesa es rechazada
-      console.error(error)
+      console.error(error);
     });
 });
